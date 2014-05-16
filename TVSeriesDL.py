29b7b0e495f9ea681a72b2d1cc704935b6749b2f
Mirco Kocher <mirco.kocher@gmx.ch>
2014-05-16 21:10:58 +0200

    updated series, ignore local files

--- TVSeriesDL.py
+++ TVSeriesDL.py
@@ -1,28 +1,29 @@
 #!C:/Python27/python.exe -u
-
+ 
 import mechanize
 import re
 import webbrowser
 import datetime
 import os
-
+ 
 username = "u"
 password = "pw"
-
+ 
 basedlURL = "http://hralinks.info/viewtopic.php?f=3&t="
-dl = {"himym": "14", "ng": "253", "tbbt": "54", "tm": "55", "taahm": "18", "got": "93", "2bg": "270", "ts": "83"}
+dl = {"ng": "253", "tbbt": "54", "tm": "55", "taahm": "18", "got": "93", "2bg": "270", "ts": "83", "caso": "737"}
 baseinfoURL = "http://services.tvrage.com/feeds/episodeinfo.php?sid="
-info = {"himym": "3918", "ng": "28304", "tbbt": "8511", "tm": "18967", "taahm": "6454", "got": "24493", "2bg": "28416", "ts": "6190"}
-
+info = {"ng": "28304", "tbbt": "8511", "tm": "18967", "taahm": "6454", "got": "24493", "2bg": "28416", "ts": "6190", "caso": "29336"}
+ 
 loginURL = "http://hralinks.info/ucp.php?mode=login"
-
-localPath = "D:\\dl\\Series\\"
-
+ 
 # Mo = 0; Su = 6
 dayinweek = datetime.datetime.today().weekday()
-
+ 
 browser = mechanize.Browser()
-
+browser.set_handle_robots(False)
+browser.set_handle_equiv(False)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+ 
 def login():
     print "Login to hralinks..."
     browser.open(loginURL)
@@ -36,7 +37,7 @@ def login():
     browser.method = "POST"
     browser.action = loginURL
     response = browser.submit()
-
+ 
 def download(name):
     print "Fetching dl link..."
     myURL = basedlURL + dl[name]
@@ -48,11 +49,11 @@ def download(name):
         webbrowser.open(link)
     else:
         print "Link not found"
-
+ 
 def setClipboard(text):
     command = 'echo ' + text.strip() + '| clip'
     os.system(command)
-
+ 
 def getLatestNumber(name):
     myURL = baseinfoURL + info[name]
     infoResp = browser.open(myURL).read()
@@ -61,7 +62,7 @@ def getLatestNumber(name):
     n = number.replace(number[0:3], "")
     print "Latest Number: " + n
     return n
-
+ 
 def getLatestTitle(name):
     myURL = baseinfoURL + info[name]
     infoResp = browser.open(myURL).read()
@@ -69,49 +70,41 @@ def getLatestTitle(name):
     t = m.group().replace("<title>", "").replace("</title>", "")
     print "Latest Title: " + t
     return t
-
-def getMyLatestNumber(name):
-    s = name.upper()
-    if len(os.listdir(localPath + s)) == 1:
-        return 0
-    latest = os.listdir(localPath + s)[-2]
-    n = latest[0:2]
-    print "My Latest Number: " + n
-    return n
-
-
+ 
+ 
 seriesName = []
 if dayinweek == 1:
-    seriesName.append("himym")
-    print "himym"
-    seriesName.append("2bg")
-    print "2bg"
+    #seriesName.append("2bg")
+    print "2bg -> september 2014"
 if dayinweek == 2:
-    seriesName.append("ng")
-    print "ng"
+    #seriesName.append("ng")
+    print "ng -> september 2014"
 if dayinweek == 4:
-    seriesName.append("tbbt")
-    print "tbbt"
-    seriesName.append("taahm")
-    print "taahm"
+    #seriesName.append("tbbt")
+    print "tbbt -> september 2014"
+    #seriesName.append("taahm")
+    print "taahm -> september 2014"
 if dayinweek == 0:
     seriesName.append("tm")
     print "tm"
     seriesName.append("ts")
     print "ts"
-    #seriesName.append("got")
-    print "got - finished"
-
+    seriesName.append("got")
+    print "got"
+    seriesName.append("caso")
+    print "caso"
+ 
 if len(seriesName) != 0:
     for s in seriesName:
         print "Checking " + s
-        lnr = getLatestNumber(s)
-        if lnr != getMyLatestNumber(s):
-            if seriesName.index(s) == 0:
-                login()
-            download(s)
-            setClipboard(lnr + " " + getLatestTitle(s))
-            if len(seriesName) > 1:
-                raw_input("Enter for next")
-
+        if seriesName.index(s) == 0:
+            login()
+        download(s)
+        try:
+            setClipboard(getLatestNumber(s) + " " + getLatestTitle(s))
+        except:
+            print "name/number not loaded"
+        if len(seriesName) > 1:
+            raw_input("Enter for next")
+ 
 raw_input("Finished")
