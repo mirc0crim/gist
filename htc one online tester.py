0a76db03fa2457942ced34f6181bbd385dd58bbe
Mirco Kocher <mirco.kocher@gmx.ch>
2014-05-16 21:07:58 +0200

    check for new htc one, and the cool cover

--- htc one online tester.py
+++ htc one online tester.py
@@ -3,15 +3,11 @@
 import mechanize
 import re
 
-fust = "http://www.fust.ch/de/nav/shop/online-shop/category/handy/product/htc-8.html?cHash=f0aa3eee7194cf1a9430b9790b54dede"
-interdiscount = "http://www.interdiscount.ch/idshop/product/Telefone_Mobile/897927_ER-8810785046529/HTC_One_silver/detail.jsf"
-digitec = "https://www.digitec.ch/ProdukteDetails2.aspx?Reiter=Details&Artikel=265446"
-mediamarkt = "http://shop.mediamarkt.ch/de/computer-telefonie/telefonie/handys-smartphones/htc-one-silber/idp5i1s4iopq"
-steg = "http://www.steg-electronics.ch/de/article/htc-one-silver-756200.aspx"
-brack = "http://www.brack.ch/htc-one-32gb-galacial-silver-239573"
+m8 = "https://www.digitec.ch/ProdukteDetails2.aspx?Reiter=Details&Artikel=298690"
+dotView = "https://www.digitec.ch/ProdukteDetails2.aspx?Reiter=Details&Artikel=300186"
 
 browser = mechanize.Browser()
-# I guess I'm allowed to pare a publicly available document twice a day. The robots
+# I guess I'm allowed to parse a publicly available document twice a day. The robots
 # RFC states it very clearly that it is outside the scope of its memo.
 browser.set_handle_robots(False)
 
@@ -26,14 +22,12 @@ def getPage(link):
 def parseCHF(resp):
     if resp == "not available":
         return resp
-    m = re.search("CHF \d{3}\.(\d{2}|-)", str(resp))
+    m = re.search("CHF \d{2,3}\.(\d{2}|-)", str(resp))
     return m.group().replace("-", "00")
 
 print
-print "fust", parseCHF(getPage(fust)), "and 10% off"
-print "interdiscount", parseCHF(getPage(interdiscount)), "and 10% off"
-print "digitec", parseCHF(getPage(digitec))
-print "mediamarkt", parseCHF(getPage(mediamarkt))
+print "m8", parseCHF(getPage(m8))
+print "dotView", parseCHF(getPage(dotView))
+print
 
-print "steg", re.search("\d{3}\.\d{4}", str(getPage(steg))).group().replace("00", "0")
-print "brack", re.search("\d{3}\.\d{2}", str(getPage(brack))).group(), "and 1% off"
+raw_input("Finished")
